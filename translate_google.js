4b345988ff6cad6b404ef3f0bb59a0ec50e7ae35
srikar.ch <srikar.0896@gmail.com>
2016-03-14 07:09:08 +0530

    translation API via google translate

---
+++ translate_google.js
@@ -0,0 +1,31 @@
+var lang = "en";
+var currentClass = "currentLang";
+
+// Loads the language lib
+google.load("language",1);
+
+// called when the DOM is ready....
+window.addEvent("domready",function(){
+	// Retrieve the DIV to be translated.
+	var translateDiv = document.id("languageBlock");
+	// Define a function to switch from the currentlanguage to another
+	var callback = function(result) {
+		if(result.translation) {
+			translateDiv.set("html",result.translation);
+		}
+	};
+	// Add a click listener to update the DIV
+	$$("#languages a").addEvent("click",function(e) {
+		// Stop the event
+		if(e) e.stop();
+		// Get the "to" language
+		var toLang = this.get("rel");
+		// Set the translation into motion
+		google.language.translate(translateDiv.get("html"),lang,toLang,callback);
+		// Set the new language
+		lang = toLang;
+		// Add class to current
+		this.getSiblings().removeClass(currentClass);
+		this.addClass(currentClass);
+	});
+});
